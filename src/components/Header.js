331ec183e62c8f24e9a9357f05bba1d2ea128906
marcbrown2024 <marcbrown2024@gmail.com>
2023-09-23 00:32:43 -0400

    First commit -- Disney

---
+++ src/components/Header.js
@@ -0,0 +1,105 @@
+import React from "react";
+import styled from "styled-components";
+
+function Header() {
+  return (
+    <Nav>
+      <Logo src="/images/logo.svg" />
+      <NavMenu>
+        <a href="">
+          <img src="/images/home-icon.svg" />
+          <span>Home</span>
+        </a>
+        <a href="">
+          <img src="/images/search-icon.svg" />
+          <span>Search</span>
+        </a>
+        <a href="">
+          <img src="/images/watchlist-icon.svg" />
+          <span>WatchList</span>
+        </a>
+        <a href="">
+          <img src="/images/original-icon.svg" />
+          <span>Originals</span>
+        </a>
+        <a href="">
+          <img src="/images/movie-icon.svg" />
+          <span>Movies</span>
+        </a>
+        <a href="">
+          <img src="/images/series-icon.svg" />
+          <span>Series</span>
+        </a>
+      </NavMenu>
+      <UserImg src="/images/userImage.jpg"></UserImg>
+    </Nav>
+  );
+}
+
+export default Header;
+
+const Nav = styled.nav`
+  height: 70px;
+  background: #090b13;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0 36px;
+`;
+
+const Logo = styled.img`
+  width: 90px;
+  cursor: pointer;
+`;
+
+const NavMenu = styled.div`
+  display: flex;
+
+  a {
+    display: flex;
+    align-items: center;
+    text-decoration: none;
+    text-transform: uppercase;
+    padding: 0 12px;
+
+    img {
+      height: 20px;
+      padding-right: 5px;
+    }
+
+    span {
+      font-size: 13px;
+      letter-spacing: 1.42px;
+      position: relative;
+
+      &:after {
+        content: "";
+        height: 2px;
+        background: white;
+        position: absolute;
+        left: 0;
+        right: 0;
+        bottom: -6px;
+        opacity: 0;
+        transform: scaleX(0);
+        transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
+      }
+    }
+    &:hover {
+      span:after {
+        opacity: 1;
+        transform: scaleX(1);
+      }
+    }
+    &:visited {
+      color: white;
+    }
+  }
+`;
+
+const UserImg = styled.img`
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+  cursor: pointer;
+`;
---
+++ src/components/ImgSlider.js
@@ -0,0 +1,46 @@
+import React from "react";
+import styled from "styled-components";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+function ImgSlider() {
+  let settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+  };
+
+  return (
+    <Carousel {...settings}>
+      <Wrap>
+        <img src="/images/slider-badging.jpg" />
+      </Wrap>
+      <Wrap>
+        <img src="/images/slider-badag.jpg" />
+      </Wrap>
+      <Wrap>
+        <img src="/images/slider-scale.jpg" />
+      </Wrap>
+      <Wrap>
+        <img src="/images/slider-scales.jpg" />
+      </Wrap>
+    </Carousel>
+  );
+}
+
+export default ImgSlider;
+
+const Carousel = styled(Slider)`
+  margin-top: 20px;
+`;
+
+const Wrap = styled.div`
+  img {
+    width: 100%;
+    height: 100%;
+  }
+`;
